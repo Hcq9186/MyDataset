e84ce1e2bf1cb043f51a70fb9370547f466f17b6
Hcq9186 <1095275755@qq.com>
2022-04-16 17:09:16 +0800

    12

--- main_1_to_4.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.system("python 1_百度爬图.py")
-os.system("python 2_去除模糊的图片.py")
-os.system("python 3_图片去重.py")
-os.system("python 4_图片格式转化.py")
---
+++ main_3_to_5.py
@@ -0,0 +1,6 @@
+import os
+
+# os.system("python 1_百度爬图.py") # 光标指向该行，按 ctrl + / 进行注释和解开注释
+os.system("python 3_去除模糊的图片.py")
+os.system("python 4_图片去重.py")
+os.system("python 5_图片格式转化.py")
